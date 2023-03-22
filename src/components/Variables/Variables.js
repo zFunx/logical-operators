74ce80261ec16fcc8c2c19f94b571d15921c54b9
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-22 17:23:51 +0530

    Adding vars

---
+++ src/components/Variables/Variables.js
@@ -0,0 +1,10 @@
+
+const Variables = () => {
+    return (
+        <div className='bg-slate-800 p-4'>
+            dfdsf
+        </div>
+    )
+}
+
+export default Variables
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import Nav from '@/components/nav'
+import Variables from '@/components/Variables/Variables'
 
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-slate-900 flex flex-col">
       <Nav />
+      <div className="flex flex-1">
+        <Variables />
+      </div>
     </div>
   )
 }
