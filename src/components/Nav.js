c59613e1937e3b2ef44db936b37c81a0667b9908
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-22 16:50:02 +0530

    Add nav

---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav className="bg-sky-700 px-4 py-2 text-slate-200 font-bold text-lg">Logic Operators</nav>
+  )
+}
+
+export default Nav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import Nav from '@/components/nav'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="w-screen h-screen bg-slate-900 flex flex-col">
+      <Nav />
+    </div>
   )
 }
