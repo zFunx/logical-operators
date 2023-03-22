b01d27f8cd8f9b3535d2efcdbe71dbb2abd246f2
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-22 20:57:20 +0530

    Passing variable name from top

--- src/components/Input/VariableInp.js
+++ src/components/Input/VariableInp.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import { PropTypes } from "prop-types"
+
 import BoolCheckbox from '@/components/BoolCheckbox'
 
-const VariableInp = () => {
+const VariableInp = props => {
     const [isTrue, setIsTrue] = useState(false);
     const toggleTruth = () => setIsTrue(prev => !prev)
 
     return (
         <div className='p-2 bg-slate-700 rounded-md flex'>
-            <input className='bg-transparent outline-none' type="text" />
-            <BoolCheckbox isTrue={isTrue} toggleTruth={toggleTruth} />
+            <input className='bg-transparent outline-none' type="text" defaultValue={props.name} />
+            <BoolCheckbox isTrue={props.val} toggleTruth={toggleTruth} />
         </div>
     )
+}
+
+VariableInp.propTypes = {
+    name: PropTypes.string.isRequired,
+    val: PropTypes.bool.isRequired
 }
 
 export default VariableInp
--- src/components/Variables/Variables.js
+++ src/components/Variables/Variables.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 import VariableInp from '@/components/Input/VariableInp'
 
 const Variables = () => {
+    const [variables, setVariables] = useState({
+        'My Arg': false,
+    })
+    const setTruth = (varName, val) => {
+
+    }
+
     return (
         <div className='bg-slate-800 p-4 h-full text-slate-200'>
             <h2>My variables</h2>
             <div className='py-4'>
-                <VariableInp />
+                {Object.keys(variables).map(varName => <VariableInp key={varName} name={varName} val={variables[varName]} />)}
             </div>
         </div>
     )
