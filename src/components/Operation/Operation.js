b8fafba153652d0e64121491fce17c53324a4899
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-25 11:41:09 +0530

    Block level logic

--- src/components/Operation/Operation.js
+++ src/components/Operation/Operation.js
@@ -7,14 +7,15 @@ const createOperand = (operand, variables, ops) => {
     } else if (operand.arg) {
         return <VariableDropdown variableName={operand.arg} isTrue={variables[operand.arg]} />
     } else if (operand.op) {
-        return <Operation operator={ops[operand.op].operator} operands={ops[operand.op].operands} variables={variables} ops={ops} />
+        // return <Operation result={ops[operand.op].} operator={ops[operand.op].operator} operands={ops[operand.op].operands} variables={variables} ops={ops} />
+        return <Operation {...ops[operand.op]} ops={ops} variables={variables} />
     }
 }
 
 const Operation = ({ operator, operands, ...props }) => {
     return (
         <div className="border-l-2 border-slate-700 hover:border-slate-500 ml-2 my-2 pl-2 text-slate-100 w-28">
-            <div className="font-bold ">{operator}</div>
+            <div className="font-bold ">{operator} {props.result.toString()}</div>
             {operands.map(operand => <div key={operand.id} className='p-1'>{createOperand(operand, props.variables, props.ops)}</div>)}
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Nav from '@/components/nav'
 import Variables from '@/components/Variables/Variables'
@@ -11,15 +11,59 @@ export default function Home() {
   })
   const [ops, setOps] = useState({
     firstOp: {
+      result: false,
       operator: 'or',
       operands: [{ id: 'a1', val: false }, { id: 'a2', val: true }, { id: 'b1', arg: 'My Arg' }, { id: 'c1', op: 'second' }],
     },
     second: {
+      result: false,
       operator: 'and',
       operands: [{ id: 'd1', val: false }, { id: 'd2', val: true }, { id: 'e2', arg: 'My Arg' }]
     }
   }
   )
+
+  const getBoolFromOperand = (operand) => {
+    if (operand.val?.toString()) {
+      return operand.val
+    } else if (operand.arg) {
+      return variables[operand.arg]
+    } else if (operand.op) {
+      return ops[operand.op].result
+    }
+  }
+
+  useEffect(() => {
+    if (ops) {
+      const tempOps = ops;
+      Object.keys(tempOps).reverse().map(key => {
+        if ('or' == tempOps[key].operator) {
+          let result = false;
+          tempOps[key].operands.some((operand) => {
+            const val = getBoolFromOperand(operand)
+            if (val) {
+              result = true
+              return true;
+            }
+          });
+
+          tempOps[key].result = result;
+        } else if ('and' == tempOps[key].operator) {
+          let result = true;
+          tempOps[key].operands.some((operand) => {
+            const val = getBoolFromOperand(operand)
+            if (!val) {
+              result = false
+              return true;
+            }
+          });
+          
+          tempOps[key].result = result;
+        }
+      })
+    }
+  }, [ops])
+
 
   return (
     <div className="w-screen h-screen bg-slate-900 flex flex-col">
@@ -29,8 +73,8 @@ export default function Home() {
           <Variables variables={variables} setVariables={setVariables} />
         </div>
         {/* <div> */}
-          <Operation {...ops.firstOp} ops={ops} variables={variables} />
-          {/* <Dropdown options={['Constant', 'Argument', 'AND', 'OR']} label="+ Add" /> */}
+        <Operation {...ops.firstOp} ops={ops} variables={variables} />
+        {/* <Dropdown options={['Constant', 'Argument', 'AND', 'OR']} label="+ Add" /> */}
         {/* </div> */}
       </div>
     </div>
