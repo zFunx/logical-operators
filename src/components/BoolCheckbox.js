e7f34a8177e9a3dbe7c5f1cb40a1b0c09e1ad70d
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-27 00:05:38 +0530

    Refactor checkbox ui

--- src/components/BoolCheckbox.js
+++ src/components/BoolCheckbox.js
@@ -5,7 +5,7 @@ import xImage from "@/assets/circle-xmark-solid.svg"
 
 const BoolCheckbox = props => {
     return (
-        <div className="w-6 h-6 bg-neutral-200 rounded-full cursor-pointer"><img width={24} onClick={props.toggleTruth} src={props.isTrue ? checkImage.src: xImage.src} /></div>
+        <img className="w-6 h-6 bg-neutral-200 rounded-full cursor-pointer" width={24} onClick={props.toggleTruth} src={props.isTrue ? checkImage.src: xImage.src} />
     )
 }
 
