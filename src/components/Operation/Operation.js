5446f97fab08b45fb7db15290a5a2612cce56283
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-26 23:48:54 +0530

    Refactor operation ui

--- src/components/Operation/Operation.js
+++ src/components/Operation/Operation.js
@@ -1,3 +1,4 @@
+// Components
 import BoolToogleButton from '@/components/Button/BoolToogleButton.js'
 import VariableDropdown from '@/components/Dropdown/VariableDropdown.js'
 import WithDelButton from '@/components/WithDelButton.js'
@@ -14,16 +15,16 @@ const createOperand = (operand, variables, ops, results, updateConstant, rootUpd
             <VariableDropdown variableName={operand.arg} isTrue={variables[operand.arg]} options={variables} updateArg={(val) => updateVar(operand.id, val)} />
         </WithDelButton>
     } else if (operand.op) {
-        return <Operation operatorKey={operand.op} {...ops[operand.op]} ops={ops} variables={variables} result={results[operand.op]} results={results} updateConstant={rootUpdateConstant} updateVar={rootUpdateVar} delOp={delOp} parentkey={parentkey} delArgOrConstant={delArgOrConstant} numOfOperandsInParent={numOfOperandsInParent} createConstant={createConstant} createArg={createArg} createOp={createOp} updateOp={updateOp}/>
+        return <Operation operatorKey={operand.op} {...ops[operand.op]} ops={ops} variables={variables} result={results[operand.op]} results={results} updateConstant={rootUpdateConstant} updateVar={rootUpdateVar} delOp={delOp} parentkey={parentkey} delArgOrConstant={delArgOrConstant} numOfOperandsInParent={numOfOperandsInParent} createConstant={createConstant} createArg={createArg} createOp={createOp} updateOp={updateOp} />
     }
 }
 
 const Operation = ({ operator, operands, ...props }) => {
     return (
-        <div className={`border-l-2 ${props.result ? 'border-green-700 hover:border-green-500' : 'border-red-700 hover:border-red-500'} ml-2 my-2 pl-4 w-28`}>
-            <div className={`font-bold flex gap-2 w-max cursor-pointer ${props.result ? 'text-green-500' : 'text-red-500'}`}><div onClick={() => props.updateOp(props.operatorKey, operator == 'and' ? 'or' : 'and')}><span className='uppercase'>{operator}</span> | <span className='capitalize'>{props.result.toString()}</span></div> {props.numOfOperandsInParent > 2 && <DelButton onClick={() => props.delOp(props.parentkey, props.operatorKey)} />}</div>
+        <div className={`border-l-2 ${props.result ? 'border-green-700 hover:border-green-500' : 'border-red-700 hover:border-red-500'} ml-2 my-2 pl-4`}>
+            <div className={`font-bold flex gap-2 cursor-pointer ${props.result ? 'text-green-500' : 'text-red-500'}`}><div onClick={() => props.updateOp(props.operatorKey, operator == 'and' ? 'or' : 'and')}><span className='uppercase'>{operator}</span> | <span className='capitalize'>{props.result.toString()}</span></div> {props.numOfOperandsInParent > 2 && <DelButton onClick={() => props.delOp(props.parentkey, props.operatorKey)} />}</div>
             {operands.map(operand => <div key={operand.id} className='p-1'>{createOperand(operand, props.variables, props.ops, props.results, (operandId, val) => props.updateConstant(props.operatorKey, operandId, val), props.updateConstant, (operandId, val) => props.updateVar(props.operatorKey, operandId, val), props.updateVar, props.delOp, props.operatorKey, props.delArgOrConstant, operands.length, props.createConstant, props.createArg, props.createOp, props.updateOp)}</div>)}
-            <div className='ml-0 mt-2'>
+            <div className='mt-2'>
                 <Dropdown options={['Constant', 'Argument', 'AND', 'OR']} label="+ Add"
                     createConstant={() => props.createConstant(props.operatorKey)}
                     createArg={() => props.createArg(props.operatorKey)}
