33e339f1a3338572def18f5b502b4ef00ad2d270
zFunx <20762882+zFunx@users.noreply.github.com>
2023-03-26 22:56:51 +0530

    UI refactor

--- src/components/Button/BoolToogleButton.js
+++ src/components/Button/BoolToogleButton.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
-
 import BoolCheckbox from '@/components/BoolCheckbox'
 
 const BoolToogleButton = props => {
-    // const [isTrue, setisTrue] = useState(false);
     const toggleTruth = () => props.setIsTrue(!props.isTrue)
 
     return (
-        <div className={`${props.isTrue ? 'bg-green-700/20' : 'bg-red-700/20'} p-2 rounded-md flex gap-2 justify-between cursor-pointer`} onClick={toggleTruth}>
+        <div className={`${props.isTrue ? 'bg-green-700/20' : 'bg-red-700/20'} p-2 rounded-md flex gap-2 cursor-pointer`} onClick={toggleTruth}>
             {props.isTrue ? 'True': 'False'}
             <BoolCheckbox isTrue={props.isTrue} />
         </div>
--- src/components/Button/DelButton.js
+++ src/components/Button/DelButton.js
@@ -2,7 +2,7 @@ import delImage from "@/assets/trash-can-solid.svg"
 
 const DelButton = (props) => {
   return (
-    <img onClick={props.onClick} className="mr-2 cursor-pointer opacity-70 hover:opacity-100" width={14} src={delImage.src} />
+    <img onClick={props.onClick} className="cursor-pointer opacity-70 hover:opacity-100" width={14} src={delImage.src} />
   )
 }
 
